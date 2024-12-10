69f9d2840d324e614d19849a9cffc5cc1643e39d
Ganesh Chavan <ganeshchavan2055@gmail.com>
2024-12-09 23:57:02 -0800

    remaining changes done

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./components/website/layouts/Layout";
 import Home from "./components/website/Home";
+import Signup from "./components/Signup";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" Component={Layout}>
           <Route index Component={Home} />
+          <Route path="signup" Component={Signup} />
         </Route>
       </Routes>
     </BrowserRouter>
