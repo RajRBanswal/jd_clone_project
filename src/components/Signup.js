518b6bc4eade93c0283ef8a1e434246a3f4ac739
Ganesh Chavan <ganeshchavan2055@gmail.com>
2024-12-09 23:59:50 -0800

    all changes done

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -11,13 +11,13 @@ const Signup = () => {
                 <div className='col-lg-6 col-12 text-center m-auto mt-lg-0 mt-5'>
                     <h1 style={{ color: "#fd7e14" }}>Sign-Up</h1>
                     
-                    <input type="text" placeholder="Enter Name" name="name" required />
-                    <input type="text" placeholder="Enter Email" name="email" required />
-                    <input type="text" placeholder="Address" name="address" required />
-                    <input type="password" placeholder="Enter Password" name="password" required />
-                    <input type="password" placeholder="Repeat Password" name="repeat password" required />
+                    <input type="text" placeholder="Enter Name" name="name" className='form-control form-control-lg mt-3' required />
+                    <input type="text" placeholder="Enter Email" name="email" required className='form-control form-control-lg mt-3' />
+                    <input type="text" placeholder="Address" name="address" required  className='form-control form-control-lg mt-3'/>
+                    <input type="password" placeholder="Enter Password" name="password" required className='form-control form-control-lg mt-3' />
+                    <input type="password" placeholder="Repeat Password" name="repeat password" className='form-control form-control-lg mt-3' required />
 
-                    <button className='sign-up-button'>Sign-Up</button>
+                    <button className='btn btn-warning btn-lg mt-3'>Sign-Up</button>
 
                     <p className='py-2'> If you already have a account? <Link to='/login' style={{ color: "#fd7e14" }}>Login Now</Link></p>
                 </div>
